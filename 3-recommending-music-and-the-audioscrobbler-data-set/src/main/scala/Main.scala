51bd70ba48a6c7a5995d397f38f76be6e563bccb
Josiah Berkebile <praenato14@gmail.com>
2016-10-04 23:56:23 -0500

    simple hello-world to test spark dependencies

--- 3-recommending-music-and-the-audioscrobbler-data-set/src/main/scala/Main.scala
+++ 3-recommending-music-and-the-audioscrobbler-data-set/src/main/scala/Main.scala
@@ -1,1 +1,16 @@
 import org.apache.spark._
+
+object Main {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("Recommending Music Using ALS Collaborative Filtering")
+      .setMaster("local")
+
+    val sc = new SparkContext(conf)
+
+    println("Hello, world!")
+
+    sc.stop()
+  }
+}
+
+
