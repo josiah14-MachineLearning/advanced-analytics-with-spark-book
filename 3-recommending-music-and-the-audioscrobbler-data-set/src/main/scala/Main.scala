fd641b13965352d3a4e7487fcc4000be61fe7c16
Josiah Berkebile <praenato14@gmail.com>
2016-10-05 20:12:33 -0500

    read in the files and do some processing

--- 3-recommending-music-and-the-audioscrobbler-data-set/src/main/scala/Main.scala
+++ 3-recommending-music-and-the-audioscrobbler-data-set/src/main/scala/Main.scala
@@ -1,4 +1,5 @@
 import org.apache.spark._
+import scala.util.{Try, Success, Failure}
 
 object Main {
   def main(args: Array[String]) {
@@ -7,10 +8,25 @@ object Main {
 
     val sc = new SparkContext(conf)
 
-    println("Hello, world!")
+    val rawUserArtistData = sc.textFile("hdfs:///user/josiah/profiledata_06-May-2005/user_artist_data.txt", 8)
+    // print(rawUserArtistData.map(_.split(' ')(0).toDouble).stats())
+    val rawArtistData = sc.textFile("hdfs:///user/josiah/profiledata_06-May-2005/artist_data.txt", 8)
+    val artistById = rawArtistData.flatMap { line =>
+      val (id, name) = line.span(_ != '\t')
+      name.isEmpty match {
+        case true  => None
+        case false => {
+          val result = Try(Some((id.toInt, name.trim))) recoverWith { e: Throwable => e match {
+            case e: NumberFormatException => Success(None)
+            case e: Throwable             => Failure(throw e)
+          }}
+          result.get
+        }
+      }
+    }
+
+    println(artistById.lookup(6803336).head)
 
     sc.stop()
   }
 }
-
-
