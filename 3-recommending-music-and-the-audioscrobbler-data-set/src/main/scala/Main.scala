2536be3f5fadddf35aa8cb5c8949b832d0450334
Josiah Berkebile <praenato14@gmail.com>
2017-03-25 20:10:10 -0500

    delint and type warnings

--- 3-recommending-music-and-the-audioscrobbler-data-set/src/main/scala/Main.scala
+++ 3-recommending-music-and-the-audioscrobbler-data-set/src/main/scala/Main.scala
@@ -1,51 +1,71 @@
 import org.apache.spark._
+import org.apache.spark.rdd.RDD
+import org.apache.spark.broadcast.Broadcast
 import scala.util.{Try, Success, Failure}
 import org.apache.spark.mllib.recommendation._
+import scala.collection.Map
+import scala.collection.immutable.StringOps
+import scala.collection.mutable.ArrayOps
+import scala.Option.option2Iterable
+import scala.{Array, Option}
 
 object Main {
   def main(args: Array[String]) {
-    val conf = new SparkConf().setAppName("Recommending Music Using ALS Collaborative Filtering")
-      .setMaster("local")
+    val conf: SparkConf = new SparkConf()
+        .setAppName("Recommending Music Using ALS Collaborative Filtering")
+        .setMaster("local")
 
-    val sc = new SparkContext(conf)
+    val sc: SparkContext = new SparkContext(conf)
 
-    val rawUserArtistData = sc.textFile("hdfs:///user/josiah/profiledata_06-May-2005/user_artist_data.txt", 16)
-    val rawArtistData = sc.textFile("hdfs:///user/josiah/profiledata_06-May-2005/artist_data.txt", 16)
-    val artistById = rawArtistData.flatMap { line =>
-      val (id, name) = line.span(_ != '\t')
+    val rawUserArtistData: RDD[String] =
+      sc.textFile("hdfs:///user/josiah/profiledata_06-May-2005/user_artist_data.txt", 16)
+    val rawArtistData: RDD[String] =
+      sc.textFile("hdfs:///user/josiah/profiledata_06-May-2005/artist_data.txt", 16)
+
+    val artistById: RDD[(Int, String)] = rawArtistData.flatMap { line =>
+      val (id: String, name: String) = line.span(_ != '\t')
       name.isEmpty match {
-        case true  => None
+        case true  => option2Iterable(None)
         case false => {
-          val result = Try(Some((id.toInt, name.trim))) recoverWith { e: Throwable => e match {
-            case e: NumberFormatException => Success(None)
-            case e: Throwable             => Failure(e)
-          }}
-          result.get
+          val result: Try[Option[(Int, String)]] =
+            Try(Some((augmentString(id).toInt, name.trim))) recoverWith {
+              e: Throwable => e match {
+                case e: NumberFormatException => Success(None)
+                case e: Throwable             => Failure(e)
+            }}
+          option2Iterable(result.get)
         }
       }
     }
-    val rawArtistAlias = sc.textFile("hdfs:///user/josiah/profiledata_06-May-2005/artist_alias.txt", 16)
-    val artistAlias = rawArtistAlias.flatMap { line =>
-      val tokens = line.split('\t')
+
+    val rawArtistAlias: RDD[StringOps] =
+      sc.textFile("hdfs:///user/josiah/profiledata_06-May-2005/artist_alias.txt", 16)
+        .map(augmentString(_))
+
+    val artistAlias: Map[Int, Int] = rawArtistAlias.flatMap { line =>
+      val tokens: ArrayOps[StringOps] =
+        refArrayOps(line.split('\t')).map(s => augmentString(s))
+
       tokens(0).isEmpty match {
-        case true  => None
-        case false => Some((tokens(0).toInt, tokens(1).toInt))
+        case true  => option2Iterable(None)
+        case false => option2Iterable(Some((tokens(0).toInt, tokens(1).toInt)))
       }
     }.collectAsMap()
 
-    val bArtistAlias = sc.broadcast(artistAlias)
+    val bArtistAlias: Broadcast[Map[Int, Int]] = sc.broadcast(artistAlias)
 
-    val trainData = rawUserArtistData.map { line =>
-      val Array(userId, artistId, count) = line.split(' ').map(_.toInt)
-      val finalArtistId = bArtistAlias.value.getOrElse(artistId, artistId)
-      Rating(userId, finalArtistId, count)
+    val trainData: RDD[Rating] = rawUserArtistData.map { line =>
+      val Array(userId: Int, artistId: Int, count: Int) =
+        line.split(' ').map(_.toInt)
+      val finalArtistId: Int = bArtistAlias.value.getOrElse(artistId, artistId)
+      Rating(userId, finalArtistId, count.toDouble)
     }.cache()
 
-    val model = ALS.trainImplicit(trainData, 10, 5, 0.01, 1.0)
+    val model: MatrixFactorizationModel = ALS.trainImplicit(trainData, 10, 5, 0.01, 1.0)
 
-    println(artistById.lookup(6803336).head)
-    println(artistById.lookup(1000010).head)
-    println(model.userFeatures.mapValues(_.mkString(", ")).first())
+    println("ARTIST" + artistById.lookup(6803336).head)
+    println("ARTIST" + artistById.lookup(1000010).head)
+    println("FEATURES" + model.userFeatures.mapValues(_.mkString(", ")).first())
 
     sc.stop()
   }
